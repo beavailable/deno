40c36f98765d6bf147f10a8c44ec018e6c6d0c2e
beavailable <beavailable@proton.me>
2025-12-27 11:11:00 +0800

    Init

---
+++ update.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/bash
+set -euo pipefail
+shopt -s inherit_errexit
+
+# $@: arguments
+_curl() {
+    curl -sSL --fail-early --fail-with-body "$@"
+}
+# $1: version
+download_binaries() {
+    local f
+    for f in 'deno-x86_64-unknown-linux-gnu.zip' 'deno-aarch64-unknown-linux-gnu.zip'; do
+        _curl -O "https://github.com/denoland/deno/releases/download/v$1/$f"
+        unzip "$f"
+        if [[ "$f" == deno-x86_64* ]]; then
+            mv deno deno_amd64
+        else
+            mv deno deno_arm64
+        fi
+        rm "$f"
+    done
+}
+
+read -r version revision <<<$(sed -nE '1s/^\S+ \((\S+)-(\S+)\) .+$/\1 \2/p' debian/changelog)
+new_version=''
+tags=$(_curl "https://api.github.com/repos/denoland/deno/tags" | sed -nE 's/^\s+"name": "v(\S+)",$/\1/p')
+for tag in $tags; do
+    if [[ "$tag" == "$version" ]]; then
+        break
+    fi
+    if dpkg --compare-versions "$tag" gt "$version"; then
+        new_version="$tag"
+        break
+    fi
+done
+
+if [[ -n "$new_version" ]]; then
+    download_binaries "$new_version"
+    new_version="$new_version-1"
+elif [[ "${DENO_FORCE_RELEASE:-}" == 'true' ]]; then
+    download_binaries "$version"
+    new_version="$version-$((revision + 1))"
+else
+    exit
+fi
+
+changelog=$(cat debian/changelog)
+{
+    echo "deno ($new_version) unstable; urgency=medium"
+    echo
+    echo '  * New release.'
+    echo
+    echo " -- beavailable <beavailable@proton.me>  $(date '+%a, %d %b %Y %H:%M:%S %z')"
+    echo
+    echo "$changelog"
+} >debian/changelog
+
+user='github-actions[bot]'
+email='41898282+github-actions[bot]@users.noreply.github.com'
+git -c user.name="$user" -c user.email="$email" commit -am "Release $new_version" --author "$GITHUB_ACTOR <$GITHUB_ACTOR_ID+$GITHUB_ACTOR@users.noreply.github.com>"
+git -c user.name="$user" -c user.email="$email" tag "$new_version" -am "Release $new_version"
+git push origin --follow-tags --atomic
+
+git add deno_*
+git -c user.name="$user" -c user.email="$email" commit -m 'Add files'
+
+echo "release=true" >>$GITHUB_OUTPUT
